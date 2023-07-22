be26dc1cdef5b6a87a870a9c09da23da8edead62
Alexander Piskun <bigcat88@icloud.com>
2023-07-22 15:37:27 +0300

    fixed Selenium test(2)
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/ui_file_actions_test.py
+++ tests/ui_file_actions_test.py
@@ -6,10 +6,7 @@ import pytest
 from gfixture import NC_APP
 from PIL import Image
 from selenium import webdriver
-from selenium.common.exceptions import (
-    ElementNotInteractableException,
-    NoSuchElementException,
-)
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as exp_cond
@@ -28,8 +25,9 @@ def test_register_ui_file_actions():
     im = BytesIO()
     Image.linear_gradient("L").resize((768, 768)).save(im, format="PNG")
     try:
-        NC_APP.files.upload("tmp.png", bytes(im.getbuffer()))
-        tmp_png = NC_APP.files.by_path("tmp.png")
+        NC_APP.files.makedirs("test_ui_action", exist_ok=True)
+        NC_APP.files.upload("test_ui_action/tmp.png", bytes(im.getbuffer()))
+        tmp_png = NC_APP.files.by_path("test_ui_action/tmp.png")
         NC_APP.ui_files_actions.register("test_ui_action_im", "UI TEST Image", "/ui_action_test", mime="image")
         NC_APP.ui_files_actions.register("test_ui_action_txt", "UI TEST Txt", "/ui_action_test", mime="text")
         NC_APP.ui_files_actions.register("test_ui_action_any", "UI TEST Any", "/ui_action_test")
@@ -45,33 +43,16 @@ def test_register_ui_file_actions():
             nc_url = environ["NEXTCLOUD_URL"]
             nc_url = nc_url.replace("index.php/", "")
             nc_url = nc_url.removesuffix("/")
-            driver.get(nc_url + "/index.php/apps/files/")
+            driver.get(nc_url + "/index.php/apps/files/?dir=/test_ui_action")
             WebDriverWait(driver, 15.0).until(exp_cond.url_contains("apps/files"))
-            for i in range(30):
-                try:
-                    driver.find_element(By.ID, "app-content-files")
-                    break
-                except NoSuchElementException:
-                    pass
-                sleep(0.5)
-            page_len = len(driver.page_source)
-            for i in range(8):
-                try:
-                    driver.find_element(By.ID, "app-content-files").send_keys(Keys.ARROW_DOWN)
-                except ElementNotInteractableException:
-                    pass
-                sleep(0.5)
-                if len(driver.page_source) == page_len:
-                    break
-                page_len = len(driver.page_source)
-            sleep(5)
+            sleep(2.5)
             tmp_png_s = driver.find_element(By.XPATH, f'//a[contains(@href,"openfile={tmp_png.info["fileid"]}")]')
             items = tmp_png_s.find_elements(By.TAG_NAME, "a")
             for i in items:
                 if i.accessible_name == "Actions":
                     driver.execute_script("arguments[0].click();", i)
                     break
-            sleep(5)
+            sleep(2)
             driver.find_element(By.XPATH, '//a[contains(@data-action,"test_ui_action_any")]')
             driver.find_element(By.XPATH, '//a[contains(@data-action,"test_ui_action_im")]')
             with pytest.raises(NoSuchElementException):
@@ -82,7 +63,7 @@ def test_register_ui_file_actions():
         NC_APP.ui_files_actions.unregister("test_ui_action_txt")
         NC_APP.ui_files_actions.unregister("test_ui_action_any")
     finally:
-        NC_APP.files.delete("tmp.png", not_fail=True)
+        NC_APP.files.delete("test_ui_action", not_fail=True)
 
 
 def test_unregister_ui_file_actions():
