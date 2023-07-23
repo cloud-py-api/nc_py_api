77792c86939334d2b9852f73f88e4a393bed6335
Alexander Piskun <bigcat88@icloud.com>
2023-07-23 16:13:38 +0300

    require_capabilities tests
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -1,6 +1,7 @@
 import pytest
+from gfixture import NC_APP
 
-from nc_py_api import NextcloudException, check_error
+from nc_py_api import NextcloudException, check_error, misc
 
 
 @pytest.mark.parametrize("code", (995, 996, 997, 998, 999, 1000))
@@ -19,3 +20,17 @@ def test_nc_exception_to_str():
         raise NextcloudException(status_code=666, reason=reason, info=info)
     except NextcloudException as e:
         assert str(e) == f"[666] {reason} <{info}>"
+
+
+@pytest.mark.skipif(NC_APP is None, reason="Test assumes the AppEcosystem is installed")
+def test_require_capabilities():
+    misc.require_capabilities("app_ecosystem_v2", NC_APP.capabilities)
+    misc.require_capabilities(["app_ecosystem_v2", "theming"], NC_APP.capabilities)
+    with pytest.raises(NextcloudException):
+        misc.require_capabilities("non_exist_capability", NC_APP.capabilities)
+    with pytest.raises(NextcloudException):
+        misc.require_capabilities(["non_exist_capability", "app_ecosystem_v2"], NC_APP.capabilities)
+    with pytest.raises(NextcloudException):
+        misc.require_capabilities(
+            ["non_exist_capability", "non_exist_capability2", "app_ecosystem_v2"], NC_APP.capabilities
+        )
