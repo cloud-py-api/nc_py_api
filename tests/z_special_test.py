15934b78817d2473915c834ab58bb9ddd26baa13
Alexander Piskun <bigcat88@icloud.com>
2023-07-29 17:37:07 +0300

    dev
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/z_special_test.py
+++ tests/z_special_test.py
@@ -2,25 +2,25 @@ from os import environ, path
 from subprocess import run
 
 import pytest
-from gfixture import NC_TO_TEST
+from gfixture import NC
 
-from nc_py_api import Nextcloud, NextcloudException
+from nc_py_api import NextcloudException
 
 # These tests should be run only on GitHub and only in special environment.
 
 
 @pytest.mark.skipif("NC_AUTH_USER" not in environ or "NC_AUTH_PASS" not in environ, reason="Needs login & paasword.")
-@pytest.mark.skipif(not isinstance(NC_TO_TEST[:1][0], Nextcloud), reason="Not available for NextcloudApp.")
+@pytest.mark.skipif(NC is None, reason="Not available for NextcloudApp.")
 @pytest.mark.skipif(environ.get("CI", None) is None, reason="run only on GitHub")
-def test_password_confirmation(nc):
+def test_password_confirmation():
     # patch "PasswordConfirmationMiddleware.php" decreasing asking before Password Confirmation from 30 min to 15 secs
     patch_path = path.join(path.dirname(path.abspath(__file__)), "data/nc_pass_confirm.patch")
     cwd_path = path.dirname((path.dirname(path.dirname(path.dirname(path.abspath(__file__))))))
     print(cwd_path)
     run(["patch", "-p", "-i", patch_path], cwd=cwd_path, check=True)
     try:
-        nc.users.create("test_cover_user_spec", password="ThisIsA54StrongPassword013")
+        NC.users.create("test_cover_user_spec", password="ThisIsA54StrongPassword013")
     except NextcloudException:
         pass
-    nc.users.delete("test_cover_user_spec")
+    NC.users.delete("test_cover_user_spec")
     run(["git", "apply", "-R", patch_path], cwd=cwd_path, check=True)
