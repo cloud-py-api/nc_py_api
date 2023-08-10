2c66125e90d2fa84a56b3d2a28a7c8636fa6e623
Alexander Piskun <bigcat88@icloud.com>
2023-08-10 17:14:00 +0300

    fixed test(2)
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/_install_only_enabled_handler.py
+++ tests/_install_only_enabled_handler.py
@@ -15,11 +15,13 @@ def enabled_handler(_enabled: bool, _nc: NextcloudApp) -> str:
 @APP.on_event("startup")
 def initialization():
     ex_app.set_enabled_handler(APP, enabled_handler)
+    ex_app.enable_heartbeat(APP)
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "_install:APP",
+        "_install_only_enabled_handler:APP",
         host=environ.get("APP_HOST", "127.0.0.1"),
         port=int(environ.get("APP_PORT", 9009)),
+        log_level="warning",
     )
