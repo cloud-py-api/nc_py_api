bedbdd843f563fb1b515bd24e706ca429641c3d3
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2023-08-16 20:21:29 +0300

    UiActionFileInfo: added `to_fs_node` method. (#85)
    ```python3
    
    def convert_video_to_gif(input_file: FsNode, nc: NextcloudApp):
        save_path = path.splitext(input_file.user_path)[0] + ".gif"
        pass
    
    @APP.post("/video_to_gif")
    async def video_to_gif(
        file: UiFileActionHandlerInfo,
        nc: Annotated[NextcloudApp, Depends(nc_app)],
        background_tasks: BackgroundTasks,
    ):
        background_tasks.add_task(convert_video_to_gif, file.actionFile.to_fs_node(), nc)
        return Response()
    ```
    
    Allowing to create ``FsNode`` from ``UiFileActionHandlerInfo``.
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/ui_files_test.py
+++ tests/ui_files_test.py
@@ -6,6 +6,8 @@ import pytest
 from gfixture import NC_APP
 from PIL import Image
 
+from nc_py_api import FilePermissions, FsNode, NextcloudExceptionNotFound, ex_app
+
 pytest.importorskip("selenium", reason="Selenium is not installed")
 
 from selenium import webdriver
@@ -14,8 +16,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as exp_cond
 from selenium.webdriver.support.wait import WebDriverWait
-
-from nc_py_api import NextcloudExceptionNotFound
 
 if NC_APP is None or "app_ecosystem_v2" not in NC_APP.capabilities:
     pytest.skip("app_ecosystem_v2 is not installed.", allow_module_level=True)
@@ -77,3 +77,60 @@ def test_unregister_ui_file_actions():
     NC_APP.ui.files_dropdown_menu.unregister("test_ui_action")
     with pytest.raises(NextcloudExceptionNotFound):
         NC_APP.ui.files_dropdown_menu.unregister("test_ui_action", not_fail=False)
+
+
+def test_ui_file_to_fs_node():
+    def ui_action_check(directory: str, fs_object: FsNode):
+        permissions = 0
+        if fs_object.is_readable:
+            permissions += FilePermissions.PERMISSION_READ
+        if fs_object.is_updatable:
+            permissions += FilePermissions.PERMISSION_UPDATE
+        if fs_object.is_creatable:
+            permissions += FilePermissions.PERMISSION_CREATE
+        if fs_object.is_deletable:
+            permissions += FilePermissions.PERMISSION_DELETE
+        if fs_object.is_shareable:
+            permissions += FilePermissions.PERMISSION_SHARE
+        file_info = ex_app.UiActionFileInfo(
+            fileId=fs_object.info.fileid,
+            name=fs_object.name,
+            directory=directory,
+            etag=fs_object.etag,
+            mime="",
+            fileType="dir" if fs_object.is_dir else "file",
+            mtime=int(fs_object.info.last_modified.timestamp()),
+            size=fs_object.info.size,
+            favorite=str(fs_object.info.favorite),
+            permissions=permissions,
+            userId=fs_object.user,
+            shared="true" if fs_object.is_shared else "false",
+        )
+        fs_node = file_info.to_fs_node()
+        assert isinstance(fs_node, FsNode)
+        assert fs_node.etag == fs_object.etag
+        assert fs_node.name == fs_object.name
+        assert fs_node.user_path == fs_object.user_path
+        assert fs_node.full_path == fs_object.full_path
+        assert fs_node.file_id == fs_object.info.fileid
+        assert fs_node.is_dir == fs_object.is_dir
+        # assert fs_node.mime == fs_object.mime
+        assert fs_node.info.permissions == fs_object.info.permissions
+        assert fs_node.info.last_modified == fs_object.info.last_modified
+        assert fs_node.info.favorite == fs_object.info.favorite
+        assert fs_node.info.content_length == fs_object.info.content_length
+        assert fs_node.info.size == fs_object.info.size
+        assert fs_node.info.fileid == fs_object.info.fileid
+
+    NC_APP.files.makedirs("some folder", exist_ok=True)
+    NC_APP.files.upload("some folder/zero", content="")
+    NC_APP.files.upload("some folder/test_root.txt", content="content!")
+    try:
+        for each_file in NC_APP.files.listdir():
+            ui_action_check(directory="/", fs_object=each_file)
+        sub_dir = NC_APP.files.listdir("some folder")
+        assert sub_dir
+        for each_file in sub_dir:
+            ui_action_check(directory="/some folder", fs_object=each_file)
+    finally:
+        NC_APP.files.delete("some folder")
