a275b6b158e50545c66c7542419274d4d0b9ac6c
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2023-10-03 18:53:31 +0300

    improved calendar test (#139)
    Hope this will fix fail tests in AppAPI repo related to calendar and
    `nc_py_api`
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/actual_tests/calendar_test.py
+++ tests/actual_tests/calendar_test.py
@@ -8,16 +8,19 @@ def test_create_delete(nc):
         pytest.skip("caldav package is not installed")
 
     principal = nc.cal.principal()
-    calendars = principal.calendars()
-    assert calendars
-    calendar = calendars[0]
-    all_events_before = calendar.events()
-    event = calendar.save_event(
-        dtstart=datetime.datetime.now(),
-        dtend=datetime.datetime.now() + datetime.timedelta(hours=1),
-        summary="NcPyApi + CalDAV test",
-    )
-    all_events_after = calendar.events()
-    assert len(all_events_after) == len(all_events_before) + 1
-    event.delete()
-    assert len(calendar.events()) == len(all_events_before)
+    calendar = principal.make_calendar("test_nc_py_api")
+    try:
+        calendars = principal.calendars()
+        assert calendars
+        all_events_before = calendar.events()
+        event = calendar.save_event(
+            dtstart=datetime.datetime.now(),
+            dtend=datetime.datetime.now() + datetime.timedelta(hours=1),
+            summary="NcPyApi + CalDAV test",
+        )
+        all_events_after = calendar.events()
+        assert len(all_events_after) == len(all_events_before) + 1
+        event.delete()
+        assert len(calendar.events()) == len(all_events_before)
+    finally:
+        calendar.delete()
