4ff158eefb70afd2947f84cbd9a3e913a38ffd14
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2023-10-06 22:26:20 +0300

    docs: fixed missed CSS (#143)
    Also made Docs a bit wider
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,11 @@ nitpick_ignore_regex = [
 
 autodoc_member_order = "bysource"
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["resources"]
+
 
 def setup(app):
     app.add_js_file("js/script.js")
