33141a6d359349d16df5f9774ccb758098ff4785
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2023-11-27 13:09:06 +0300

    added "--wait-finish" for occ app_api:app:register (AppAPI 1.3+) (#166)
    Adjustments to nc_py_api API if needed will come later, this PR should
    make pass test here:
    
    https://github.com/cloud-py-api/app_api/pull/128
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- scripts/dev_register.sh
+++ scripts/dev_register.sh
@@ -14,7 +14,7 @@ echo $! > /tmp/_install.pid
 python3 tests/_install_wait.py "http://localhost:9009/heartbeat" "\"status\":\"ok\"" 15 0.5
 docker exec "$1" sudo -u www-data php occ app_api:app:register nc_py_api manual_install --json-info \
   "{\"appid\":\"nc_py_api\",\"name\":\"nc_py_api\",\"daemon_config_name\":\"manual_install\",\"version\":\"1.0.0\",\"secret\":\"12345\",\"host\":\"host.docker.internal\",\"scopes\":{\"required\":[\"SYSTEM\", \"FILES\", \"FILES_SHARING\"],\"optional\":[\"USER_INFO\", \"USER_STATUS\", \"NOTIFICATIONS\", \"WEATHER_STATUS\", \"TALK\", \"TALK_BOT\", \"ACTIVITIES\", \"NOTES\"]},\"port\":9009,\"protocol\":\"http\",\"system_app\":1}" \
-  --force-scopes
+  --force-scopes --wait-finish
 cat /tmp/_install.pid
 kill -15 "$(cat /tmp/_install.pid)"
 echo "nc_py_api for $1 is ready to use"
