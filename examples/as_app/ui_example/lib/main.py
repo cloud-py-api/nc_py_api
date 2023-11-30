abd72f77d968b610a88064141cafcf1dbfbc8fd3
Alexander Piskun <bigcat88@icloud.com>
2023-11-30 11:48:33 +0300

    test: receive value from button
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- examples/as_app/ui_example/lib/main.py
+++ examples/as_app/ui_example/lib/main.py
@@ -1,9 +1,11 @@
 """Example with which we test UI elements."""
 
+import random
 import typing
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI, responses
+from pydantic import BaseModel
 
 from nc_py_api import NextcloudApp
 from nc_py_api.ex_app import nc_app, run_app, set_handlers
@@ -23,12 +25,17 @@ def enabled_handler(enabled: bool, _nc: NextcloudApp) -> str:
     return ""
 
 
+class Button1Format(BaseModel):
+    initial_value: str
+
+
 @APP.post("/verify_initial_value")
-def verify_initial_value(
+async def verify_initial_value(
     _nc: typing.Annotated[NextcloudApp, Depends(nc_app)],
+    input1: Button1Format,
 ):
-    # print(param)
-    return responses.JSONResponse(content={"initial_value": "Button was pressed"}, status_code=200)
+    print("Old value: ", input1.initial_value)
+    return responses.JSONResponse(content={"initial_value": str(random.randint(0, 100))}, status_code=200)
 
 
 if __name__ == "__main__":
