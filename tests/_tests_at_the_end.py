657bfa12d9afe8a57915350abf752cbb5ca5401a
Alexander Piskun <bigcat88@icloud.com>
2023-12-14 18:02:05 +0300

    added async tests for ex_app_enable/ex_app_disable
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/_tests_at_the_end.py
+++ tests/_tests_at_the_end.py
@@ -9,8 +9,7 @@ from ._install_wait import check_heartbeat
 # These tests will be run separate, and at the end of all other tests.
 
 
-@pytest.mark.parametrize("file_to_test", ("_install_only_enabled_handler.py", "_install_only_enabled_handler_async.py"))
-def test_ex_app_enable_disable(nc_client, nc_app, file_to_test):
+def _test_ex_app_enable_disable(file_to_test):
     child_environment = os.environ.copy()
     child_environment["APP_PORT"] = os.environ.get("APP_PORT", "9009")
     r = Popen(
@@ -19,6 +18,12 @@ def test_ex_app_enable_disable(nc_client, nc_app, file_to_test):
         cwd=os.getcwd(),
     )
     url = f"http://127.0.0.1:{child_environment['APP_PORT']}/heartbeat"
+    return r, url
+
+
+@pytest.mark.parametrize("file_to_test", ("_install_only_enabled_handler.py", "_install_only_enabled_handler_async.py"))
+def test_ex_app_enable_disable(nc_client, nc_app, file_to_test):
+    r, url = _test_ex_app_enable_disable(file_to_test)
     try:
         if check_heartbeat(url, '"status":"ok"', 15, 0.3):
             raise RuntimeError(f"`{file_to_test}` can not start.")
@@ -29,6 +34,25 @@ def test_ex_app_enable_disable(nc_client, nc_app, file_to_test):
         nc_client.apps.ex_app_enable("nc_py_api")
         assert nc_client.apps.ex_app_is_disabled("nc_py_api") is False
         assert nc_client.apps.ex_app_is_enabled("nc_py_api") is True
+    finally:
+        r.terminate()
+        r.wait(timeout=10)
+
+
+@pytest.mark.asyncio(scope="session")
+@pytest.mark.parametrize("file_to_test", ("_install_only_enabled_handler.py", "_install_only_enabled_handler_async.py"))
+async def test_ex_app_enable_disable_async(anc_client, anc_app, file_to_test):
+    r, url = _test_ex_app_enable_disable(file_to_test)
+    try:
+        if check_heartbeat(url, '"status":"ok"', 15, 0.3):
+            raise RuntimeError(f"`{file_to_test}` can not start.")
+        if await anc_client.apps.ex_app_is_enabled("nc_py_api"):
+            await anc_client.apps.ex_app_disable("nc_py_api")
+        assert await anc_client.apps.ex_app_is_disabled("nc_py_api") is True
+        assert await anc_client.apps.ex_app_is_enabled("nc_py_api") is False
+        await anc_client.apps.ex_app_enable("nc_py_api")
+        assert await anc_client.apps.ex_app_is_disabled("nc_py_api") is False
+        assert await anc_client.apps.ex_app_is_enabled("nc_py_api") is True
     finally:
         r.terminate()
         r.wait(timeout=10)
