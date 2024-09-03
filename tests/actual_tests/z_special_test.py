fb3ee4904526f5c407f7de3c255dcd05331c700f
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-09-04 00:12:31 +0300

    CI: "password_confirmation" test fix (#291)
    This is only half of fix for CI, "test_conversation_avatar" test should
    fail still and need fix
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- tests/actual_tests/z_special_test.py
+++ tests/actual_tests/z_special_test.py
@@ -13,7 +13,7 @@ from nc_py_api import NextcloudException
 @pytest.mark.skipif("NC_AUTH_USER" not in environ or "NC_AUTH_PASS" not in environ, reason="Needs login & paasword.")
 @pytest.mark.skipif(environ.get("CI", None) is None, reason="run only on GitHub")
 def test_password_confirmation(nc_client):
-    patch_path, cwd_path = _test_password_confirmation()
+    patch_path, cwd_path = _test_password_confirmation(nc_client.srv_version["major"])
     nc_client.update_server_info()
     old_adapter = nc_client._session.adapter
     with contextlib.suppress(NextcloudException):
@@ -27,7 +27,7 @@ def test_password_confirmation(nc_client):
 @pytest.mark.skipif("NC_AUTH_USER" not in environ or "NC_AUTH_PASS" not in environ, reason="Needs login & paasword.")
 @pytest.mark.skipif(environ.get("CI", None) is None, reason="run only on GitHub")
 async def test_password_confirmation_async(anc_client):
-    patch_path, cwd_path = _test_password_confirmation()
+    patch_path, cwd_path = _test_password_confirmation((await anc_client.srv_version)["major"])
     await anc_client.update_server_info()
     old_adapter = anc_client._session.adapter
     with contextlib.suppress(NextcloudException):
@@ -37,9 +37,12 @@ async def test_password_confirmation_async(anc_client):
     run(["git", "apply", "-R", patch_path], cwd=cwd_path, check=True)
 
 
-def _test_password_confirmation() -> tuple[str, str]:
+def _test_password_confirmation(nc_version) -> tuple[str, str]:
     # patch "PasswordConfirmationMiddleware.php" decreasing asking before Password Confirmation from 30 min to 5 secs
-    patch_path = path.join(path.dirname(path.dirname(path.abspath(__file__))), "data/nc_pass_confirm.patch")
+    if nc_version <= 30:
+        patch_path = path.join(path.dirname(path.dirname(path.abspath(__file__))), "data/nc_pass_confirm.patch")
+    else:
+        patch_path = path.join(path.dirname(path.dirname(path.abspath(__file__))), "data/nc_pass_confirm_nc31.patch")
     cwd_path = path.dirname(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
     run(["patch", "-p", "1", "-i", patch_path], cwd=cwd_path, check=True)
     sleep(6)
